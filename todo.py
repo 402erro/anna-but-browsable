39e2b0df760b4da061350ca8f64abd4c5b0c0ae5
Rowdha <rowdhafakier@gmail.com>
2025-08-19 00:37:10 +0200

    Added a todo. Prb gonna do it never (jk)

---
+++ todo.py
@@ -0,0 +1,4 @@
+# Redo get_book_info into a function so i can import its values and stuff to other files
+# Start messing with torrents. 
+# Get MD5 -> Match MD5 to seekable json -> get torrent name and bytestart -> dynamically get torrent btih and magnet link
+# use qbittorent web api to add torrent using magnet then seek through the torrent to find the book's file
